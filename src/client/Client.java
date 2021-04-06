93596105deaf0d2a87def5f352c4526bc0e00924
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-04-07 01:23:25 +1000

    Can now create TCP connection with server

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,14 +5,26 @@ import java.net.Socket;
 public class Client {
 	String hostname;
 	int port;
+	static ClientGUI clientWindow;
 	
 	public static void main(String[] args) throws ExceptionHandling {
 		System.out.print("Hello from client\n");
 		
-		if (args.length != 2) {
-			throw new ExceptionHandling("Input error", "Not enough input");
-		}
+//		if (args.length != 2) {
+//			throw new ExceptionHandling("Input error", "Not enough input");
+//		}
+		
+		clientWindow = new ClientGUI();
+		clientWindow.frame.setVisible(true);
+		System.out.print("After initializing client gui\n");
+		
 	}
+	
+	
+	
+//	public Client(String txt){
+//		System.out.println(txt);
+//	}
 }
 
 
--- src/client/ClientGUI.java
+++ src/client/ClientGUI.java
@@ -5,32 +5,99 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClientGUI {
 
-	private JFrame frame;
+	protected JFrame frame;
+	private JTextField textField;
+	
+	private static String hostname;
+	private int port;
+	
+	Socket socket = null;
+	// Get the input/output streams for reading/writing data from/to the socket
+	BufferedReader in;
+	BufferedWriter out;
 
 	/**
 	 * Launch the application.
+	 * @throws ExceptionHandling 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExceptionHandling {
+		System.out.print("Hello from client\n");
+		
+		if (args.length != 2) {
+			throw new ExceptionHandling("Input error", "Not enough input");
+		}
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ClientGUI window = new ClientGUI();
+					ClientGUI window = new ClientGUI(args);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+		
 	}
 
 	/**
 	 * Create the application.
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 */
-	public ClientGUI() {
+	public ClientGUI(String[] args) throws UnknownHostException, IOException {
 		initialize();
+		hostname = args[0];
+		port = Integer.parseInt(args[1]);
+		createTCPConnection();
+	}
+	
+	
+	/**
+	 * Create TCP connection
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 */
+	private void createTCPConnection() throws UnknownHostException, IOException {		
+		socket = new Socket(hostname, port);
+		// Get the input/output streams for reading/writing data from/to the socket
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+		out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+	}
+	
+	
+	/**
+	 * Send word to the dictionary server
+	 * @param word
+	 * @throws IOException
+	 */
+	private void sendWordToServer(String word) throws IOException {		
+		// Send the input string to the server by writing to the socket output stream
+		out.write(word + "\n");
+		out.flush();
+		System.out.println("Message sent");
 	}
 
 	/**
@@ -46,5 +113,32 @@ public class ClientGUI {
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
 		lblNewLabel.setBounds(186, 6, 78, 19);
 		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(92, 66, 130, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.setBounds(261, 66, 117, 29);
+		frame.getContentPane().add(btnNewButton);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(156, 136, 147, 79);
+		frame.getContentPane().add(textArea);
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String fetchText = textField.getText();
+				textArea.append(fetchText + "\n");
+				textField.setText("");
+				try {
+					sendWordToServer(fetchText);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 	}
 }
