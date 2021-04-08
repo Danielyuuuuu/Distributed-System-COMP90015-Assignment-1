9bd26baa2aa9d12d3272458c4ec30b39f57b4057
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-04-08 17:05:44 +1000

    Made SetPortToListen into a class that implements runnable

--- src/server/Server.java
+++ src/server/Server.java
@@ -45,16 +45,16 @@ public class Server {
 	private JFrame frame;
 	
 	private int port;
-	private ServerSocket listeningSocket = null;
+//	private ServerSocket listeningSocket = null;
 	private Socket clientSocket = null;
 	
 	private Thread listeningThread = null;
 	
-	private JLabel serverStatus = null;
+	private static JLabel serverStatus = null;
 	
 	private Hashtable<String, ArrayList<String>> dict = new Hashtable<>();
 	
-	private int counter = 0;
+	private static int counter = 0;
 	
 	/**
 	 * Launch the application.
@@ -87,17 +87,21 @@ public class Server {
 		port = Integer.parseInt(args[0]);
 		initialize();
 		
+//		// Start a new thread to listen on a specific port
+//		listeningThread = new Thread(() -> 
+//				{
+//					try {
+//						SetPortToListen();
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				});
+//		listeningThread.start();
+		
 		// Start a new thread to listen on a specific port
-		listeningThread = new Thread(() -> 
-				{
-					try {
-						SetPortToListen();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				});
-		listeningThread.start();
+		new Thread(new SetPortToListen(port)).start();
+		
 		ReadDictFile("./dict-file.json");
 	}
 	
@@ -110,22 +114,22 @@ public class Server {
         JSONObject jo = (JSONObject) obj;
         
         for(Object word : jo.keySet()) {
-        	System.out.println(word + ": " + jo.get(word));
+//        	System.out.println(word + ": " + jo.get(word));
 //        	Object[] translations = (Object [])jo.get(word);
         	ArrayList<String> translationArrayList = new ArrayList<>();
         	JSONArray array = (JSONArray) jo.get(word);
 //        	array.add(jo.get(word));
-        	System.out.println(array.size());
+//        	System.out.println(array.size());
         	for(int i = 0; i < array.size(); i++) {
         		translationArrayList.add((String) array.get(i));
         	}
         	dict.put(word.toString(), translationArrayList);
         }
         
-        System.out.println("Print dict");
-        for (String word : dict.keySet()) {
-        	System.out.println(word + ": " + dict.get(word));
-        }
+//        System.out.println("Print dict");
+//        for (String word : dict.keySet()) {
+//        	System.out.println(word + ": " + dict.get(word));
+//        }
 	}
 	
 	
@@ -244,72 +248,119 @@ public class Server {
 	}
 	
 	
-	private void SetPortToListen() throws IOException {
-		//Create a server socket listening on port
-		listeningSocket = new ServerSocket(port);
-		serverStatus.setText("Listening on port: " + port);
-		System.out.println("Server listening on port " + port +  " for a connection");
-		
-        // running infinite loop for getting
-        // client request
-        while (true) {
-
-            // socket object to receive incoming client
-            // requests
-            Socket client = listeningSocket.accept();
-            
-            counter++;
-            // Displaying that new client is connected
-            
-            // to server
-            System.out.println("New client connected: " + counter);
-
-
-            // This thread will handle the client
-            // separately
-            new Thread(new HandleClientConnection(client, counter)).start();
-        }
-		
-		
-//		//Listen for incoming connections for ever 
-//		while (listeningSocket != null && true) 
-//		{
-//			System.out.println("Server listening on port " + port +  " for a connection");
-//			//Accept an incoming client connection request 
-//			clientSocket = listeningSocket.accept(); //This method will block until a connection request is received
-//			System.out.println("Client conection accepted:");
-//			System.out.println("Remote Hostname: " + clientSocket.getInetAddress().getHostName());
-//			System.out.println("Local Port: " + clientSocket.getLocalPort());
-//			
-//			//Get the input/output streams for reading/writing data from/to the socket
-//			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
-//			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
+//	private void SetPortToListen() throws IOException {
+//		//Create a server socket listening on port
+//		listeningSocket = new ServerSocket(port);
+//		serverStatus.setText("Listening on port: " + port);
+//		System.out.println("Server listening on port " + port +  " for a connection");
+//		
+//        // running infinite loop for getting
+//        // client request
+//        while (true) {
 //
-//			
-//			//Read the message from the client and reply
-//			//Notice that no other connection can be accepted and processed until the last line of 
-//			//code of this loop is executed, incoming connections have to wait until the current
-//			//one is processed unless...we use threads!
-//			String clientMsg = null;
-//			try 
-//			{
-//				while((clientMsg = in.readLine()) != null) 
-//				{
-//					System.out.println("Message from client: " + clientMsg);
-//					out.write("Server Ack " + clientMsg + "\n");
-//					out.flush();
-//					System.out.println("Response sent");
-//				}
-//			}
-//			
-//			catch(SocketException e)
-//			{
-//				System.out.println("closed...");
-//			}
-//			
-//			clientSocket.close();
-//		}
+//            // socket object to receive incoming client
+//            // requests
+//            Socket client = listeningSocket.accept();
+//            
+//            counter++;
+//            // Displaying that new client is connected
+//            
+//            // to server
+//            System.out.println("New client connected: " + counter);
+//
+//
+//            // This thread will handle the client
+//            // separately
+//            new Thread(new HandleClientConnection(client, counter)).start();
+//        }
+//		
+//		
+////		//Listen for incoming connections for ever 
+////		while (listeningSocket != null && true) 
+////		{
+////			System.out.println("Server listening on port " + port +  " for a connection");
+////			//Accept an incoming client connection request 
+////			clientSocket = listeningSocket.accept(); //This method will block until a connection request is received
+////			System.out.println("Client conection accepted:");
+////			System.out.println("Remote Hostname: " + clientSocket.getInetAddress().getHostName());
+////			System.out.println("Local Port: " + clientSocket.getLocalPort());
+////			
+////			//Get the input/output streams for reading/writing data from/to the socket
+////			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+////			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
+////
+////			
+////			//Read the message from the client and reply
+////			//Notice that no other connection can be accepted and processed until the last line of 
+////			//code of this loop is executed, incoming connections have to wait until the current
+////			//one is processed unless...we use threads!
+////			String clientMsg = null;
+////			try 
+////			{
+////				while((clientMsg = in.readLine()) != null) 
+////				{
+////					System.out.println("Message from client: " + clientMsg);
+////					out.write("Server Ack " + clientMsg + "\n");
+////					out.flush();
+////					System.out.println("Response sent");
+////				}
+////			}
+////			
+////			catch(SocketException e)
+////			{
+////				System.out.println("closed...");
+////			}
+////			
+////			clientSocket.close();
+////		}
+//	}
+	
+	
+	private static class SetPortToListen implements Runnable {
+		//Create a server socket listening on port
+		private final ServerSocket listeningSocket;
+		private final int port;
+		
+		public SetPortToListen(int port) throws IOException {
+			this.port = port;
+			listeningSocket = new ServerSocket(port);
+		}
+		
+		public void run() {
+			serverStatus.setText("Listening on port: " + port);
+			System.out.println("Server listening on port " + port +  " for a connection");
+			
+	        // running infinite loop for getting
+	        // client request
+	        while (true) {
+	
+	            // socket object to receive incoming client
+	            // requests
+	            Socket client;
+				try {
+					client = listeningSocket.accept();
+					
+		            counter++;
+		            // Displaying that new client is connected
+		            
+		            // to server
+		            System.out.println("New client connected: " + counter);
+		
+		
+		            // This thread will handle the client
+		            // separately
+		            new Thread(new HandleClientConnection(client, counter)).start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            
+
+	        }
+		}
+		
 	}
+	
 	
 	
 	// To handle client connection 
