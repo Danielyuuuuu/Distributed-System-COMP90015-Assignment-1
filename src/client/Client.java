201fdd55655d3d62bbd07af660c32fc26412d010
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-04-17 18:44:14 +1000

    Changed the Client constructor to private

--- src/client/Client.java
+++ src/client/Client.java
@@ -52,12 +52,8 @@ public class Client {
 		
 	}
 
-	/**
-	 * Create the application.
-	 * @throws IOException 
-	 * @throws UnknownHostException 
-	 */
-	public Client() {
+
+	private Client() {
 		initializeConnectionGUI();
 	}
 	
